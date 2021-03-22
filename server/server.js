3f8b4fbffe468f34561b2dd4b1f4341fb6bc49e2
tanman <tanner.wood17@gmail.com>
2021-03-21 22:02:56 -0600

    project was completed many weeks ago but I forgot to push the last commit lol

--- server/server.js
+++ server/server.js
@@ -19,24 +19,30 @@ io.on("connection", (socket) => {
 
   socket.on("serverJoin", (username) => {
     const user = {
-      username,
+      username: username,
       id: socket.id,
+      move: null,
       room: "",
+      wins: 0,
+      losses: 0,
+      ties: 0,
     };
     users.push(user);
-    io.emit("newUser", users);
+    io.emit("usersUpdate", users);
+    socket.emit("userUpdate", getUserBySocketId(socket.id))
   });
 
   socket.on("roomJoin", (roomName) => {
-
     let success = false;
+    // console.log(`join request for ${roomName}\n occupancy:${roomOccupancy(roomName)}`)
     if (roomOccupancy(roomName) < 2) {
       socket.join(roomName);
-      getUserBySocketId(socket.id).room = roomName;
       success = true;
+      setUserRoom(socket.id, roomName);
+      io.emit("usersUpdate", users);
+      socket.emit("userUpdate", getUserBySocketId(socket.id))
     }
-
-    socket.emit("roomJoinResult", success);
+    socket.emit("roomJoinResult", { status: success, room: roomName });
   });
 
   socket.on("msg", (msg) => {
@@ -46,20 +52,67 @@ io.on("connection", (socket) => {
     // io.to(socket#id).emit('hey')
   });
 
-  socket.on("userMove", (type) => {
-    console.log(`received ${type}`);
-    socket.emit("msg", type);
+  socket.on("userMove", (move) => {
+    console.log(`received ${move}`);
+    socket.emit("msg", move);
+    let senderId = socket.id;
+    setMove(socket.id, move);
+    io.emit("usersUpdate", users);
+    socket.emit("userUpdate", getUserBySocketId(socket.id))
+    let room = getUserBySocketId(senderId).room;
+    console.log(`userMove, read senders room as ${room}`)
+    console.log(`move from socket\n${JSON.stringify(getUserBySocketId(socket.id), null, 2)}`)
+
+    // if both users have answered,
+    if (haveBothUsersAnswered(room)) {
+      console.log("detected both users have answered")
+      let opponents = getUsersByRoom(room);
+      let userA = opponents[0];
+      let userB = opponents[1];
+      console.log(`opponents\n${JSON.stringify(opponents, null, 2)}`)
+
+      // determine winner
+      let payload = results[userA.move][userB.move];
+      // user A wins
+      if (payload.winner === 0) {
+        payload.winner = userA.id;
+        incrementProperty(userA.id, "wins");
+        incrementProperty(userB.id, "losses");
+        // user B wins
+      } else if (payload.winner === 1) {
+        payload.winner = userB.id;
+        incrementProperty(userB.id, "wins");
+        incrementProperty(userA.id, "losses");
+      }
+      // tie
+      else {
+        incrementProperty(userA.id, "ties");
+        incrementProperty(userB.id, "ties");
+      }
+
+      // and send back the results
+      console.log(`results- ${JSON.stringify(payload, null, 2)}`)
+      io.emit("usersUpdate", users);
+      io.to(userA.id).emit("userUpdate", getUserBySocketId(userA.id))
+      io.to(userB.id).emit("userUpdate", getUserBySocketId(userB.id))
+      io.to(room).emit("results", payload);
+
+      // set user moves back to null
+      setMove(userA.id, null)
+      setMove(userB.id, null)
+    }
   });
 
   socket.on("disconnect", () => {
     users = users.filter((user) => {
       user.id !== socket.id;
     });
-    io.emit("newUser", users);
+    io.emit("usersUpdate", users);
+    socket.emit("userUpdate", getUserBySocketId(socket.id))
   });
 });
 
-// helpers
+// helpers /////////////////////////////////////////////////
 function roomOccupancy(room) {
   let count = 0;
   users.forEach((user) => {
@@ -69,8 +122,192 @@ function roomOccupancy(room) {
 }
 
 function getUserBySocketId(id) {
+  // console.log(`query request for user with socket id ${JSON.stringify(id, null, 2)}`)
+  // console.log(`current users ${JSON.stringify(users, null, 2)}`)
   let user = users.filter((user) => {
-    user.id === id;
+    return user.id === id;
   });
-  return user;
+  // console.log(`returned ${JSON.stringify(user[0], null, 2)}`)
+  return user[0];
 }
+
+function getUsersByRoom(room) {
+  // console.log(`looking for users in room '${room}'`)
+  let usersByRoom = []
+  users.forEach((user)=>{
+    if(user.room === room) usersByRoom.push(user)
+  })
+  // console.log(`BY ROOM:\n${JSON.stringify(usersByRoom, null, 2)}`)
+  return usersByRoom;
+}
+
+function setUserRoom(id, room) {
+  users.forEach((user) => {
+    if (user.id === id) {
+      user.room = room;
+    }
+  });
+}
+
+function setMove(id, move) {
+  // console.log(`attempting to set the move ${move} to ${id}`);
+  users.forEach((user) => {
+    if (user.id === id) {
+      // console.log("matched user, attempting to change");
+      user.move = move;
+    }
+  });
+  // console.log(JSON.stringify(users, null, 2));
+}
+
+function incrementProperty(id, prop) {
+  users.forEach((user) => {
+    if (user.id === id) {
+      user[prop]++;
+    }
+  });
+}
+
+function haveBothUsersAnswered(room) {
+  let answers = 0;
+  console.log(`getUsersByRoom:\n${JSON.stringify(getUsersByRoom(room))}`)
+  getUsersByRoom(room).forEach((user) => {
+    if (user.move !== null) answers++;
+  });
+  return answers === 2;
+}
+
+// constants ///////////////////////////////////////////////
+const rockAndScissors = "Rock crushes Scissors!";
+const rockAndLizard = "Rock crushes Lizard!";
+const paperAndRock = "Paper covers Rock!";
+const paperAndSpock = "Paper disproves Spock!";
+const scissorsAndPaper = "Scissors cuts Paper!";
+const scissorsAndLizard = "Scissors decapitates lizard!";
+const lizardAndPaper = "Lizard eats Paper!";
+const lizardAndSpock = "Lizard poisons Spock!";
+const spockAndScissors = "Spock smashes Scissors";
+const spockAndRock = "Spock vaporizes Rock!";
+const tie = "It's a tie!";
+
+const results = [
+  // player A: rock
+  [
+    {
+      winner: null,
+      message: tie,
+    },
+    {
+      winner: 1,
+      message: paperAndRock,
+    },
+    {
+      winner: 0,
+      message: rockAndScissors,
+    },
+    {
+      winner: 0,
+      message: rockAndLizard,
+    },
+    {
+      winner: 1,
+      message: spockAndRock,
+    },
+  ],
+
+  // player A: paper
+  [
+    {
+      winner: 0,
+      message: paperAndRock,
+    },
+    {
+      winner: null,
+      message: tie,
+    },
+    {
+      winner: 1,
+      message: scissorsAndPaper,
+    },
+    {
+      winner: 1,
+      message: lizardAndPaper,
+    },
+    {
+      winner: 0,
+      message: paperAndSpock,
+    },
+  ],
+
+  // player A: scissors
+  [
+    {
+      winner: 1,
+      message: rockAndScissors,
+    },
+    {
+      winner: 0,
+      message: scissorsAndPaper,
+    },
+    {
+      winner: null,
+      message: tie,
+    },
+    {
+      winner: 0,
+      message: scissorsAndLizard,
+    },
+    {
+      winner: 1,
+      message: spockAndScissors,
+    },
+  ],
+
+  // player A: lizard
+  [
+    {
+      winner: 1,
+      message: rockAndLizard,
+    },
+    {
+      winner: 0,
+      message: lizardAndPaper,
+    },
+    {
+      winner: 1,
+      message: scissorsAndLizard,
+    },
+    {
+      winner: null,
+      message: tie,
+    },
+    {
+      winner: 0,
+      message: lizardAndSpock,
+    },
+  ],
+
+  // player A: spock
+  [
+    {
+      winner: 0,
+      message: spockAndRock,
+    },
+    {
+      winner: 1,
+      message: paperAndSpock,
+    },
+    {
+      winner: 0,
+      message: spockAndScissors,
+    },
+    {
+      winner: 1,
+      message: lizardAndSpock,
+    },
+    {
+      winner: null,
+      message: tie,
+    },
+  ],
+];
--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,86 @@
 <template>
   <v-app>
     <app-bar />
-    <v-main class="blue">
+    <v-main>
       <v-container>
-        <v-card class="text-center mx-auto mt-4" dark max-width="750">
-          <v-card-text>
-            <v-row align="center">
-              <v-col>
-                <h2 class="white--text">
-                  Send the code below to a friend (on your network) to have them
-                  join your game!
-                </h2>
-                <!-- <p>{{ this.val }}</p> -->
-                <v-card
-                  flat
-                  color=""
-                  class="rounded-lg mx-auto d-flex justify-center"
-                >
-                  <v-card-title class="headline">{{ roomCode }}</v-card-title>
-                </v-card>
-                <h2 class="white--text">
-                  Or enter a code to join someone else's game
-                </h2>
-                <div class="d-flex align-center">
-                  <v-text-field
-                    class="centered mt-n4 mb-n4"
-                    max-width="20px"
-                    v-model="toRoom"
-                  ></v-text-field>
-                </div>
+        <v-card v-if="!userNameSet" dark class="white--text">
+          <v-card-title>Set your username!</v-card-title>
+
+          <v-card-actions>
+            <v-text-field
+              max-width="20px"
+              v-model="userNameInput"
+              v-on:keyup.enter="setUsername()"
+            ></v-text-field>
+            <v-checkbox v-model="saveUsername" :label="'save username?'"></v-checkbox>
+          </v-card-actions>
+        </v-card>
+
+        <div v-if="!roomOccupied">
+          <v-card class="text-center mx-auto mt-4" dark>
+            <v-card-text>
+              <v-row align="center">
+                <v-col>
+                  <h2 class="white--text">
+                    Send the code below to a friend (on your network) to have
+                    them join your game!
+                  </h2>
+                  <v-card
+                    flat
+                    color=""
+                    class="rounded-lg mx-auto d-flex justify-center"
+                  >
+                    <v-card-title class="headline">{{ roomCode }}</v-card-title>
+                  </v-card>
+                  <h2 class="white--text">
+                    Or enter a code to join someone else's game
+                  </h2>
+                  <div class="d-flex align-center">
+                    <v-text-field
+                      class="centered mt-n4 mb-n4"
+                      v-model="toRoom"
+                      v-on:keyup.enter="joinRequest()"
+                      :disabled="disableRoomChange"
+                    ></v-text-field>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </div>
+
+        <div v-if="roomOccupied">
+          <v-card dark v-if="currentRoom" class="mx-auto d-flex justify-center">
+            <v-card-title>Name: {{ user.username }}</v-card-title>
+            <v-card-title>{{ user.room }}</v-card-title>
+          </v-card>
+
+          <v-card dark v-if="currentRoom" class="mx-auto d-flex justify-center">
+            <v-card-title>You: {{ user.wins }} Opponent: {{ user.losses }}</v-card-title>
+          </v-card>
+
+          <v-card dark v-if="currentRoom">
+            <v-row no-gutters class="d-flex justify-center">
+              <v-col cols="12">
+                <v-btn small text outlined dark @click="sendRock()">Rock</v-btn>
+                <v-btn small text outlined dark @click="sendPaper()">Paper</v-btn>
+                <v-btn small text outlined dark @click="sendScissors()">Scissors</v-btn>
+                <v-btn small text outlined dark @click="sendLizard()">Lizard</v-btn>
+                <v-btn small text outlined dark @click="sendSpock()">Spock</v-btn>
+              </v-col>
+              <v-col cols="12" class="d-flex justify-center">
+                <v-btn small text outlined dark @click="sendSpeech()">Say Move</v-btn>
               </v-col>
             </v-row>
-          </v-card-text>
-        </v-card>
-        <v-card dark max-width="750px" class="mx-auto d-flex justify-center">
-          <v-btn dark @click="sendMessage()"
-            >Socket id:{{ this.socketId }}</v-btn
-          >
-          <v-btn dark @click="sendRock()">Rock</v-btn>
-          <v-btn dark @click="sendPaper()">Paper</v-btn>
-          <v-btn dark @click="sendScissors()">Scissors</v-btn>
-          <v-btn dark @click="sendLizard()">Lizard</v-btn>
-          <v-btn dark @click="sendSpock()">Spock</v-btn>
-        </v-card>
+          </v-card>
+        </div>
+
+        <div v-if="messages">
+          <v-card dark class="white--text mt-4" v-for="(message, index) in messages" :key="index">
+            <v-card-title>{{message}}</v-card-title>
+          </v-card>
+        </div>
+
       </v-container>
     </v-main>
   </v-app>
@@ -50,80 +88,178 @@
 
 <script>
 import AppBar from "./components/AppBar";
-// import { mapActions, mapGetters } from "vuex";
-// import LobbyCard from "./components/LobbyCard.vue";
 const io = require("socket.io-client");
+
+var recognition = new window.webkitSpeechRecognition();
+recognition.onspeechend = function () {
+  console.log("speech end")
+  recognition.stop();
+};
 
 export default {
   name: "App",
 
   components: {
     AppBar,
-    // LobbyCard,
   },
 
   methods: {
-    // ...mapActions(["initializeSocket"]),
 
+    randomRoom(length) {
+      let result = "";
+      let characters = "ABCDEFGHIJKLMNPQRSTUVWXYZ123456789";
+      let charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      this.roomCode = result;
+    },
     sendMessage() {
-      // console.log(`hello from vue`)
-      // this.$store.state.socket.sendMessage()
       this.socket.emit("msg", `hey im socket ${this.socket.id}`);
     },
     sendRock() {
-      this.socket.emit("userMove", "rock");
+      this.socket.emit("userMove", 0);
     },
     sendPaper() {
-      this.socket.emit("userMove", "paper");
+      this.socket.emit("userMove", 1);
     },
     sendScissors() {
-      this.socket.emit("userMove", "scissors");
+      this.socket.emit("userMove", 2);
     },
     sendLizard() {
-      this.socket.emit("userMove", "lizard");
+      this.socket.emit("userMove", 3);
     },
     sendSpock() {
-      this.socket.emit("userMove", "spock");
+      this.socket.emit("userMove", 4);
+    },
+    sendSpeech() {
+      recognition.start();
+    },
+    getCode(word) {
+      switch(word) {
+        case "rock":
+          return 0;
+        case "paper":
+          return 1;
+        case "scissors":
+          return 2;
+        case "lizard":
+          return 3;
+        case "spock":
+          return 4;
+      }
+    },
+    joinRequest() {
+      this.socket.emit("roomJoin", this.toRoom);
+      this.toRoom = "";
+    },
+    joinInitialRoom() {
+      this.socket.emit("roomJoin", this.roomCode);
+    },
+    setUsername() {
+      if (this.saveUsername) window.localStorage.setItem("savedUsername", this.userNameInput)
+
+      this.userName = this.userNameInput;
+      this.userNameSet = true;
+      this.userNameInput = "";
+      this.socket.emit("serverJoin", this.userName);
+      this.joinInitialRoom();
+      this.disableRoomChange = false;
     },
   },
 
   computed: {
-    // ...mapGetters(["socket, socketId"]),
-    // roomCode: function() {
-    //   return this.$store.state.roomCode;
-    // },
-    // socket: function() {
-    //   return this.$store.state.socket
-    // },
-    // socketId: function() {
-    //   return this.$store.state.socket.id
-    // }
+
+    roomOccupied: function() {
+      let room = this.user.room;
+      let count = 0;
+      this.users.forEach((user) => {
+        if (user.room === room) count++;
+      });
+      return count === 2;
+    },
   },
 
   data: () => ({
     socketId: null,
     socket: null,
-    roomCode: "AXY7",
-    toRoom: ""
+    roomCode: "",
+    toRoom: "",
+    currentRoom: "",
+    users: [],
+    user: {},
+    userNameSet: false,
+    userNameInput: "",
+    userName: "",
+    registered: false,
+    disableRoomChange: true,
+    messages: [],
+    saveUsername: false,
+    recognition: null
   }),
 
   mounted: function() {
-    // this.$store.dispatch('initializeSocket')
-    // this.initializeSocket()
-    console.log("attempting to initialize socket");
+
+    recognition.onresult = (event) =>{
+    console.log("on result")
+    let transcript = event.results[0][0].transcript;
+    console.log(transcript)
+    if (confirm(`Are you sure you want to send '${transcript}-${this.getCode(transcript)}'?`)){
+      this.socket.emit("userMove", this.getCode(transcript));
+    }
+    };
+
+    this.randomRoom(4);
+    let savedUserCheck = window.localStorage.getItem("savedUsername")
+    if (savedUserCheck) this.userNameInput = savedUserCheck
 
     // locally initialize socket
     if (!this.socket) {
       let socket = io.connect("http://localhost:3000/");
       socket.on("connect", () => {
-        // console.log(this.socket.id);
         this.socketId = socket.id;
       });
-      socket.on("msg", (msg) => {
-        console.log(`server-${msg}`);
+
+      socket.on("roomJoinResult", ({ status, room }) => {
+        // console.log(`client received success-${status} room-${room}`);
+        if (status === true) {
+          this.currentRoom = room;
+          // console.log("room join successful");
+        } else {
+          // console.log("room join unsuccessful");
+        }
+      });
+
+      socket.on("usersUpdate", (users) => {
+        this.users = users;
+        // console.log(`updated users\n${JSON.stringify(users, null, 2)}`);
+      });
+
+      socket.on("userUpdate", (user) => {
+        this.user = user;
+        // console.log(`updated THE user\n${JSON.stringify(user, null, 2)}`);
+      });
+
+      socket.on("results", (payload) => {
+        // console.log(`Received results!\n${JSON.stringify(payload, null, 2)}`);
+        this.messages.push(payload.message)
+
+        // speak!
+        const speaker = window.speechSynthesis
+        let phrase = new SpeechSynthesisUtterance(payload.message)
+        speaker.speak(phrase)
       });
       this.socket = socket;
     }
   },
 };
 </script>
+
+<style>
+  .v-btn__content {
+    font-size: 10px !important;
+    padding: 0px !important;
+  }
+</style>
